8f087938f6e685fd2d232a1b87cdb1e37e978d0f
Joel Jackson <jackson.joel@gmail.com>
2013-04-10 16:30:52 -0700

    Adds a scaffold rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XfireOauthConsumer::Application.config.secret_token = '47ba741377909ade30e4e9cc63e0f8db435b057e35694a87d75e414af6ca2a0c25a9cd240514e3e0d8da8e28c1c57ec5c9e052d4b5d044845601eb6fce66817e'
