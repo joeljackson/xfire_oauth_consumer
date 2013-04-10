e2eaa0d310563117013a2b272064ad7205e501d1
Joel Jackson <jackson.joel@gmail.com>
2013-04-10 16:58:17 -0700

    auth methods for devise

--- app/controllers/oauth_consumers_controller.rb
+++ app/controllers/oauth_consumers_controller.rb
@@ -4,8 +4,7 @@ class OauthConsumersController < ApplicationController
   # Replace this with the equivalent for your authentication framework
   # Eg. for devise
   #
-  #   before_filter :authenticate_user!, :only=>:index
-  before_filter :login_required, :only=>:index
+  before_filter :authenticate_user!, :only=>:index
 
   def index
     @consumer_tokens=ConsumerToken.all :conditions=>{:user_id=>current_user.id}
@@ -33,9 +32,9 @@ class OauthConsumersController < ApplicationController
   # The plugin requires logged_in? to return true or false if the user is logged in. Uncomment and
   # call your auth frameworks equivalent below if different. eg. for devise:
   #
-  # def logged_in?
-  #   user_signed_in?
-  # end
+  def logged_in?
+    user_signed_in?
+  end
 
   # The plugin requires current_user to return the current logged in user. Uncomment and
   # call your auth frameworks equivalent below if different.
@@ -46,14 +45,14 @@ class OauthConsumersController < ApplicationController
   # The plugin requires a way to log a user in. Call your auth frameworks equivalent below
   # if different. eg. for devise:
   #
-  # def current_user=(user)
-  #   sign_in(user)
-  # end
+  def current_user=(user)
+    sign_in(user)
+  end
 
   # Override this to deny the user or redirect to a login screen depending on your framework and app
   # if different. eg. for devise:
   #
-  # def deny_access!
-  #   raise Acl9::AccessDenied
-  # end
+  def deny_access!
+    raise Acl9::AccessDenied
+  end
 end
